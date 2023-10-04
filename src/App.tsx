173654d8daae9e1dec6dea5e577f69825ebc8114
Tulio Marcos <tuliosilva@ntopus.com.br>
2023-10-04 20:14:42 -0300

    feat: add logic MovieCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,75 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import { Header } from 'components/Header'
-import { MovieCatalog } from 'components/MovieCatalog'
+import { MovieCard } from 'components/MovieCard'
 import { ThemeProvider } from 'components/ThemeProvider'
-import { LoadSpinner } from 'components/Spinner'
+import { Spinner } from 'components/Spinner'
+
+
+const dataMock = [
+  {
+    id: "1",
+    image: "filme1.jpg",
+    title: "Matrix",
+    price: 12.99,
+  },
+  {
+    id: "2",
+    image: "filme2.jpg",
+    title: "Star Wars: Episódio IV - Uma Nova Esperança",
+    price: 14.99,
+  },
+  {
+    id: "3",
+    image: "filme3.jpg",
+    title: "Pulp Fiction",
+    price: 9.99,
+  },
+  {
+    id: "4",
+    image: "filme4.jpg",
+    title: "O Senhor dos Anéis: A Sociedade do Anel",
+    price: 15.99,
+  }
+]
+
+interface Cart {
+  id: string
+  image: string
+  title: string
+  price: number
+  quantity: number
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [isLoading, setIsLoading] = useState(true)
+  const [cart, setCart] = useState<Cart[]>([])
+  const [isLoading] = useState(false)
+  
+  const quantityOfItemSelected = useMemo(() => cart.length, [cart]) 
+
+  function handleAddToCart(movieId: string) {
+    setCart((prev) => {
+      const itemExist = prev.find(item => item.id === movieId)
+      const item = dataMock.find(item => item.id === movieId)
+      if(!itemExist) prev.push({...item!, quantity: 1})
+      return [...prev]
+    })
+  }
 
   return (
     <ThemeProvider>
-      <Header items={count}></Header>
+      <Header items={quantityOfItemSelected}></Header>
       <main>
-        { isLoading && <LoadSpinner /> }
-        { !isLoading && <MovieCatalog /> }
+        { isLoading && <Spinner /> }
+        { !isLoading && dataMock.map((item) => (
+          <MovieCard 
+            id={item.id}
+            image={item.image}
+            title={item.title}
+            price={item.price}
+            quantity={cart.find(cartItem => cartItem.id === item.id)?.quantity ?? 0}
+            onAddToCart={() => handleAddToCart(item.id)}
+          /> 
+        ))}
       </main>
     </ThemeProvider>
   )
--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,11 +1,27 @@
 import * as Styled from './MovieCard.style'
-
+import { ShoppingCart } from 'components/Icons'
 interface CardProps {
-	items: number
+  id: string
+  image: string
+  title: string
+  price: number
+  quantity: number
+  onAddToCart: () => void
 }
 
 export function MovieCard(props: CardProps) {
-	return <Styled.Container>
-		
-	</Styled.Container>
+  const isSelectedItem = props.quantity > 0
+
+  return (
+    <Styled.Container>
+      <img src={props.image} alt={props.title} />
+      <p>{props.title}</p>
+      <span>R$ {props.price}</span>
+      <button onClick={props.onAddToCart}>
+        <ShoppingCart/>
+        {props.quantity}
+        {isSelectedItem ? 'Item adicionando' : 'Adicionar ao carrinho'}
+      </button>
+    </Styled.Container>
+  )
 }
